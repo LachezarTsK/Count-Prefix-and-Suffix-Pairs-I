52e4a9339e2587e95298a16586c7a59ea2799d9e
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-03-03 09:49:25 +0100

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,23 @@
+
+
+using System;
+
+public class Solution
+{
+    public int CountPrefixSuffixPairs(string[] words)
+    {
+        int numberOfPrefixSuffixPairs = 0;
+
+        for (int i = 0; i < words.Length; ++i)
+        {
+            for (int j = i + 1; j < words.Length; ++j)
+            {
+                if (words[j].StartsWith(words[i]) && words[j].EndsWith(words[i]))
+                {
+                    ++numberOfPrefixSuffixPairs;
+                }
+            }
+        }
+        return numberOfPrefixSuffixPairs;
+    }
+}
