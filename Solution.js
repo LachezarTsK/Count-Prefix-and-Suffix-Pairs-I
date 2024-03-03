d5dccab22a4be1c83b29f4c72847afb11a5a2e30
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-03-03 09:47:47 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,18 @@
+
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var countPrefixSuffixPairs = function (words) {
+    let numberOfPrefixSuffixPairs = 0;
+
+    for (let i = 0; i < words.length; ++i) {
+        for (let j = i + 1; j < words.length; ++j) {
+
+            if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
+                ++numberOfPrefixSuffixPairs;
+            }
+        }
+    }
+    return numberOfPrefixSuffixPairs;
+};
